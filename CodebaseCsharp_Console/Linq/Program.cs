32cf957cea86bd58b5bb9321fb4cddb37748e1ba
Joe la Sardine <thibaut.poittevin@gmail.com>
2018-03-15 18:19:53 +0100

    Linq studied

--- CodebaseCsharp_Console/Linq/Program.cs
+++ CodebaseCsharp_Console/Linq/Program.cs
@@ -8,8 +8,54 @@ namespace CodebaseCsharp.Linq
 {
     class Program
     {
+        /* Standard query operators : 
+        * - where
+        * - orderby
+        * - select
+        * - from
+        * - group
+        * - let 
+        */
+
         static void Main(string[] args)
         {
+            Execute();
+            
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        static List<Student> students = new List<Student>
+        {
+           new Student {First="Svetlana",   Last="Omelchenko",  ID=111, Scores= new List<int> {97, 92, 81, 60}},
+           new Student {First="Claire",     Last="O'Donnell",   ID=112, Scores= new List<int> {75, 84, 91, 39}},
+           new Student {First="Sven",       Last="Mortensen",   ID=113, Scores= new List<int> {88, 94, 65, 91}},
+           new Student {First="Cesar",      Last="Garcia",      ID=114, Scores= new List<int> {97, 89, 85, 82}},
+           new Student {First="Debra",      Last="Garcia",      ID=115, Scores= new List<int> {35, 72, 91, 70}},
+           new Student {First="Fadi",       Last="Fakhouri",    ID=116, Scores= new List<int> {99, 86, 90, 94}},
+           new Student {First="Hanying",    Last="Feng",        ID=117, Scores= new List<int> {93, 92, 80, 87}},
+           new Student {First="Hugo",       Last="Garcia",      ID=118, Scores= new List<int> {92, 90, 83, 78}},
+           new Student {First="Lance",      Last="Tucker",      ID=119, Scores= new List<int> {68, 79, 88, 92}},
+           new Student {First="Terry",      Last="Adams",       ID=120, Scores= new List<int> {99, 82, 81, 79}},
+           new Student {First="Eugene",     Last="Zabokritski", ID=121, Scores= new List<int> {96, 85, 91, 60}},
+           new Student {First="Michael",    Last="Tucker",      ID=122, Scores= new List<int> {94, 92, 91, 91} }
+        };
+
+        static void Execute()
+        {
+            Queries queries = new Queries(students);
+
+            queries.PrintList();
+            queries.PrintNames();
+            queries.PrintNamesAplhabetically();
+            queries.PrintStudentsByAverage(85, true);
+            queries.PrintStudentsByAverage(84, false);
+            queries.PrintAverageTotal();
+        }
+
+        static void LogVar<T>(string name, T variable)
+        {
+            Console.WriteLine("La variable '{0}' est de type '{1}'.", name, typeof(T));
         }
     }
 }
---
+++ CodebaseCsharp_Console/Linq/Queries.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodebaseCsharp.Linq
+{
+    class Queries
+    {
+        List<Student> data;
+        
+        public Queries(List<Student> data)
+        {
+            this.data = data;
+        }
+
+        public void PrintList()
+        {
+            Console.WriteLine("Printing overall list :");
+
+            var query = from s in data
+                        select s;
+
+            foreach (var s in query)
+            {
+                Console.WriteLine("  {0}", s);
+            }
+            Console.WriteLine();
+        }
+
+        public void PrintNames()
+        {
+            Console.WriteLine("Printing names list :");
+
+            var query = from s in data
+                        orderby s.First
+                        select new { s.First, s.Last };
+
+            foreach (var s in query)
+            {
+                Console.WriteLine("  {0} {1}", s.First, s.Last);
+            }
+            Console.WriteLine();
+        }
+
+        public void PrintNamesAplhabetically()
+        {
+            Console.WriteLine("Printing names list :");
+            
+            var query = from s in data
+                        group s by s.Last[0] into sgroup
+                        orderby sgroup.Key
+                        select sgroup;
+
+            foreach (var group in query)
+            {
+                Console.WriteLine("  {0}", group.Key);
+                foreach (var s in group)
+                {
+                    Console.WriteLine("    {0}, {1}", s.Last, s.First);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void PrintStudentsByAverage(int treshold, bool higher)
+        {
+            Console.WriteLine("Printing names of students that have a {0} score than {1} :", higher ? "higher" : "lower", treshold);
+
+            var query = from s in data
+                        let total = s.Scores[0] + s.Scores[1] + s.Scores[2] + s.Scores[3]
+                        let average = total / 4
+                        where (average >= treshold && higher) || (average <= treshold && !higher)
+                        select "(" + average + ") " + s.First + " " + s.Last;
+
+            foreach (var s in query)
+            {
+                Console.WriteLine("  {0}", s);
+            }
+            Console.WriteLine();
+        }
+
+        public void PrintAverageTotal()
+        {
+            Console.WriteLine("Average total is :");
+
+            var query = (from s in data
+                         let total = s.Scores[0] + s.Scores[1] + s.Scores[2] + s.Scores[3]
+                         select total)
+                         .Average();
+
+            Console.WriteLine("  {0}", query);
+            Console.WriteLine();
+        }
+    }
+}
